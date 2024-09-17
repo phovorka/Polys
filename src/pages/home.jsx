c72ea6549d304143b90807ac24c4fb2daec2bcc0
Glitch (glitch-blank-react) <none>
2024-09-17 16:35:48 +0000

    📊🏡 Checkpoint ./package.json:9475993/1361 ./src/pages/home.jsx:9475993/2413 ./src/styles/styles.css:9475993/429

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <h1 className="title">{hello}</h1>
-      <p>
+      <p className="title">
         This starter gives you everything you need to start working on a new
         React project, and nothing more. If you're looking for something with
         more options included out of the box, try remixing
@@ -16,6 +16,18 @@ export default function Home() {
         </a>
         .
       </p>
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div className="md:flex">
+    <div className="md:shrink-0">
+      <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture"></img>
+    </div>
+    <div className="p-8">
+      <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+      <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
+      <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+    </div>
+  </div>
+</div>
     </>
   );
 }
