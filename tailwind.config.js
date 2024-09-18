f5cc8c7d9fb078ddc6d17790fce0b24c3e44cbf5
Glitch (glitch-blank-react) <none>
2024-09-18 11:30:33 +0000

    👖💧 Checkpoint ./tailwind.config.js:9475993/766 ./index.html:9475993/2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/pages/**/*.{html,js,jsx}',
+    './src/**/*.{html,js,jsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#ff6347',
+        secondary: '#4f46e5',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'), // Plugin pro lepší stylování formulářů
+    require('@tailwindcss/typography'), // Plugin pro lepší typografii
+  ],
 }
-
