ef7268d48dcf4ce0e6632f0f3588c9e84fef9027
Glitch (glitch-blank-react) <none>
2024-09-20 15:55:13 +0000

    🍮🌥 Checkpoint ./src/pages/foot.jsx:9475993/252 ./package.json:9475993/58

--- src/pages/foot.jsx
+++ src/pages/foot.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-yellow-500 text-white  justify-center py-6">
-      <p className="text-center">© 2024 Code Inspiration. All rights reserved.</p>
+    <footer className="bg-grey-500 text-blue  text-center  w-full">
+      <p className="">© 2024 Code Inspiration. All rights reserved.</p>
       <div className="flex justify-center mt-4 space-x-4">
         <a
           href="/privacy-policy"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
+
         >
           Privacy Policy
         </a>
@@ -17,7 +17,7 @@ const Footer = () => {
           href="/terms-of-service"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
+
         >
           Terms of Service
         </a>
