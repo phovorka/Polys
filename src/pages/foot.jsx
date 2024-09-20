73134ef1fd20223e3969e7715132319886abb77a
Glitch (glitch-blank-react) <none>
2024-09-20 15:43:09 +0000

    ⛄️🏘 Checkpoint ./src/pages/foot.jsx:9475993/290 ./tailwind.config.js:9475993/252

--- src/pages/foot.jsx
+++ src/pages/foot.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-300 justify-center text-blue py-6">
+    <footer className="bg-yellow-500 text-white  justify-center py-6">
       <p className="text-center">© 2024 Code Inspiration. All rights reserved.</p>
       <div className="flex justify-center mt-4 space-x-4">
         <a
           href="/privacy-policy"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-400 hover:underline"
+          className="text-blue-500 hover:underline"
         >
           Privacy Policy
         </a>
@@ -17,7 +17,7 @@ const Footer = () => {
           href="/terms-of-service"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-400 hover:underline"
+          className="text-blue-500 hover:underline"
         >
           Terms of Service
         </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,10 @@ module.exports = {
     './src/**/*.{html,js,jsx}',
       './index.html',
 
-    
-   
   ],
   theme: {
-    extend: {
-      fontFamily: {
-       poppins: ['Poppins', 'sans-serif'],
-      },
-      colors: {
-        primary: '#ff6347',
-        secondary: '#4f46e5',
-      },
-    },
+    extend: {},
   },
-
-}
+  plugins: [],
+};
  
