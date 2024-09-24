afaf2444c6472119a06105a2761e5f28c3b15cd6
Glitch (glitch-blank-react) <none>
2024-09-24 12:59:07 +0000

    🏉⏰ Checkpoint ./.glitch-assets:9475993/441 ./src/pages/home.jsx:9475993/183

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -60,7 +60,7 @@ const Home = () => {
 
       <div className="flex items-center space-x-4 mb-6">
         <img
-          src="https://cdn.glitch.global/2222818e-a3bd-45a1-bab7-d9b34184a3e0/code.svg?v=1727181401677"
+          src="https://cdn.glitch.global/2222818e-a3bd-45a1-bab7-d9b34184a3e0/work-forecast.svg?v=1727182701684"
           alt="Logo"
           className="w-12 h-12"
         />
