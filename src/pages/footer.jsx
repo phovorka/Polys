21f57af567e3dfb5ad80396a28f1fd5580f039b2
Glitch (glitch-blank-react) <none>
2024-09-25 10:44:58 +0000

    ⛸🌇 Checkpoint ./index.html:9475993/30 ./src/pages/footer.jsx:9475993/2956 ./src/styles/styles.css:9475993/555

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -9,17 +9,22 @@ const Footer = () => {
           href="https://docs.google.com/spreadsheets/d/1nc9iRz8njhfVdZslBrwyB52BLBYMArbwu2nnkCeBD9Q/"
           target="_blank"
           rel="noopener noreferrer"
-
         >
           Sheet link
         </a>
-        <a
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-
-        >
+        <a href="#" target="_blank" rel="noopener noreferrer">
           Polysearch
+        </a>
+        <a
+          className="btn--remix"
+          target="_top"
+          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
+        >
+          <img
+            src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140"
+            alt=""
+          />
+          Remix on Glitch
         </a>
       </div>
     </footer>
