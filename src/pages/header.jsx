68f96e0754b802a2436bce33ec6a5a8411518f90
Glitch (glitch-blank-react) <none>
2024-09-25 11:43:40 +0000

    🤘🕰 Checkpoint ./src/pages/header.jsx:9475993/195

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -8,11 +8,11 @@ const Top = () => {
       <header className="flex justify-between items-center px-8 py-4 bg-white shadow-md">
         <Link
           to="/main"
-          className="text-purple-800 font-bold text-xl  hover:text-blue-400"
-        >  <img src="https://cdn.glitch.global/2222818e-a3bd-45a1-bab7-d9b34184a3e0/Frame%201%20(2).svg?v=1727263360970"  className=" " />
+          className="flex items-center   text-purple-800 font-bold text-xl  hover:text-blue-400"
+        >  <img src="https://cdn.glitch.global/2222818e-a3bd-45a1-bab7-d9b34184a3e0/Frame%201%20(2).svg?v=1727263360970"  className=" pr-2" />
           Polysearch
         </Link>
-        <Link to="/how" className="text-purple-500 hover:text-blue-400">
+        <Link to="/how" className=" text-purple-500 hover:text-blue-400">
           How it works?
         </Link>
       </header>
