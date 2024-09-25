f7415afe64ea6d4faf22229f3a6a70635b81539e
Glitch (glitch-blank-react) <none>
2024-09-25 10:57:34 +0000

    🗼🍴 Checkpoint ./src/pages/footer.jsx:9475993/36 ./src/styles/styles.css:9475993/206

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
         <a
           className="btn--remix"
           target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
+          href="https://glitch.com/edit/#!/remix/awesome-polysearch"
         >
           <img
             src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140"
