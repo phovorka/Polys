367f2aa8d45a8b724834fe171f372b6c87a20aaf
Glitch (glitch-blank-react) <none>
2024-09-25 11:32:02 +0000

    📺🏬 Checkpoint ./.glitch-assets:9475993/1195 ./index.html:9475993/588 ./src/pages/header.jsx:9475993/381

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -9,7 +9,7 @@ const Top = () => {
         <Link
           to="/main"
           className="text-purple-800 font-bold text-xl  hover:text-blue-400"
-        >
+        >  <img src="https://cdn.glitch.global/2222818e-a3bd-45a1-bab7-d9b34184a3e0/Frame%201%20(2).svg?v=1727263360970"  className=" " />
           Polysearch
         </Link>
         <Link to="/how" className="text-purple-500 hover:text-blue-400">
